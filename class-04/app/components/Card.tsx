54c55824b0c2940e1f08f13fd452b4c991e1961c
MUHAMMADFARHANHANIF <farhansmit0318@gmail.com>
2024-10-07 15:44:01 +0500

    shadcn botton edit

--- class-04/app/components/Card.tsx
+++ class-04/app/components/Card.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
 
-const Card = (prop:{name:string,rollnumber:string,likes:string,class:string,day:string}) => {
+const Card = (prop:{img:string,name:string,rollnumber:string,likes:string,class:string,day:string}) => {
   return (
     <>
     <div className="  m-3  ">
     <div  className="bg-white shadow-lg w-72 border-2 border-dashed  rounded-lg overflow-hidden transition-transform transform hover:scale-105 duration-300">
       <div className="flex items-center p-4">
         <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRkRfjdfaUowNM2FHzZla0C-E3lheZVfS5Eg&s"
+          src={prop.img}
           alt={`${prop.name}'s profile`}
           className="w-14 h-14 rounded-full border-2 border-gray-300 mr-4 object-cover"
         />
@@ -23,7 +25,9 @@ const Card = (prop:{name:string,rollnumber:string,likes:string,class:string,day:
       <div className="flex justify-between items-center p-4 border-t bg-blue-300">
           
         <span className="text-zinc-900 font-bold">Likes: {prop.likes || 0}</span>
+        <Button>click me </Button>
       </div>
+
     </div>
   
 </div>
